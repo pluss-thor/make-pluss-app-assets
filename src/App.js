19458de6b7c33c2c3f428c09ef34b9982bec4412
Thorbjorn Hansen <tbkh91@gmail.com>
2021-09-27 15:30:57 +1000

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import PropTypes from "prop-types";
 import "./App.css";
 
-function App({ logo, backgroundColor }) {
+function App({ image, backgroundColor }) {
   return (
     <div className="App" style={{ backgroundColor }}>
-      <img src={logo} className="logo" alt="logo" />
+      <img src={image} className="logo" alt="logo" />
     </div>
   );
 }
 
 App.propTypes = {
-  logo: PropTypes.string,
+  image: PropTypes.string,
   backgroundColor: PropTypes.string,
 };
 
 App.defaultProps = {
-  logo: "https://i0.wp.com/plusscommunities.com/wp-content/uploads/2021/01/Untitled.png?fit=172%2C70&ssl=1",
+  image:
+    "https://i0.wp.com/plusscommunities.com/wp-content/uploads/2021/01/Untitled.png?fit=172%2C70&ssl=1",
   backgroundColor: "#fff",
 };
 
